d9c5f9ebbb8d1d876f6aaf60ede0707955a84fee
parthgupta9 <parth615gupta@gmail.com>
2026-01-19 20:51:24 +0530

    changes done

--- Boy/Ayesha Solution-Ankit(Boy)/server.js
+++ Boy/Ayesha Solution-Ankit(Boy)/server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Body parsing
+// Body parsing - handle both urlencoded and multipart
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
